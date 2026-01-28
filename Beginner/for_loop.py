6df0624da12e6169997dd0db124c7e19c6950a73
Yamunika Chintapanti <199625828+yamunikac@users.noreply.github.com>
2026-01-28 10:06:13 +0530

    Refactor for_loop.py with dice simulation and exercise
    Updated the dice rolling simulation and added a jumping jacks exercise.

--- Beginner/for_loop.py
+++ Beginner/for_loop.py
@@ -1,13 +1,22 @@
 import random
-# dice rolling
+
+# Dice simulation
 rolls = []
 for i in range(20):
     rolls.append(random.randint(1, 6))
-print(rolls)
-print(rolls.count(6))
-print(rolls.count(1))
-count = 0
-for i in range(len(rolls) - 1):
-    if rolls[i] == 6 and rolls[i + 1] == 6:
-        count += 1
-print(count)
+print("Dice Rolls:", rolls)
+print("Number of 1s:", rolls.count(1))
+print("Number of 6s:", rolls.count(6))
+
+
+# Jumping Jacks
+completed = 0
+for i in range(10):
+    completed += 10
+    tired = input("Are you tired? ")
+
+    if tired.lower() in ["yes", "y"]:
+        skip = input("Do you want to skip the remaining sets? ")
+        if skip.lower() in ["yes", "y"]:
+            break
+print("You completed", completed, "jumping jacks")
