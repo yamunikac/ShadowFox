e657d4901fa2f04e234559f6fde713c8ce3f1949
Yamunika Chintapanti <199625828+yamunikac@users.noreply.github.com>
2026-01-28 10:07:21 +0530

    Refactor Hangman game code structure

--- InterMediate/HangmanGame.py
+++ InterMediate/HangmanGame.py
@@ -1,32 +1,23 @@
 import random
-
 words = ["python", "developer", "internship", "programming"]
 word = random.choice(words)
-
 guessed = []
 tries = 6
-
 while tries > 0:
     display = ""
     for letter in word:
         display += letter if letter in guessed else "_"
     print(display)
-
     if "_" not in display:
         print("You won")
         break
-
     guess = input("Guess a letter: ").lower()
-
     if guess in guessed:
         print("Already guessed")
         continue
-
     guessed.append(guess)
-
     if guess not in word:
         tries -= 1
         print("Wrong guess, tries left:", tries)
-
 if tries == 0:
     print("You lost. Word was:", word)
