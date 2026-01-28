856d875d1e78e2b2a4623bfe68d2aec46fe81be7
Yamunika Chintapanti <199625828+yamunikac@users.noreply.github.com>
2026-01-28 09:40:57 +0530

    Add files via upload

---
+++ InterMediate/webScraper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://quotes.toscrape.com/"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+quotes = soup.find_all("span", class_="text")
+
+for i, quote in enumerate(quotes, 1):
+    print(i, quote.text)
