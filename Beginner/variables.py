b3c29c4a74bf9b5107cb99b258fe49f3e46596bb
Yamunika Chintapanti <199625828+yamunikac@users.noreply.github.com>
2026-01-28 10:03:44 +0530

    Reformat variables.py with consistent indentation

--- Beginner/variables.py
+++ Beginner/variables.py
@@ -4,13 +4,12 @@ print("Value of pi:", pi)
 print("Datatype of pi:", type(pi))
 
 # This program demonstrates that 'for' cannot be used as a variable name
-
 #for = 4
 
 # 1(c) Simple Interest
 principal = 5000
 rate = 5
 time = 3
-
 simple_interest = (principal * rate * time) / 100
 print("Simple Interest:", simple_interest)
+
